772d12ffe21dbb716d8ed550a3f71d2b771c3941
赖城粮 <e.laichengliang@szewec.com>
2022-03-07 14:22:28 +0800

    First commit

---
+++ love/CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "婷 I Love You",
+  descriptionHeader:
+    "你愿意做我女朋友吗？ ^^",
+  buttonYes: "我愿意",
+  buttonNo: "对不起, 我们不合适",
+  titleModar: "我的织女：",
+  descriptionModar:
+    '玫瑰花开，朵朵都是爱你的心；浮云擦过，片片都是念你的情；柳絮飘飞，带着我的浓浓深情；流星滑落，让爱你的美梦成真。情人节，牵你的手，今生一起走过，无怨无悔！',
+};
